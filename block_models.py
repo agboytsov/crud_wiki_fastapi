b2e504d7216290a07fe524c0a45ce4904e8ae151
agboytsov <133656833+agboytsov@users.noreply.github.com>
2024-03-18 16:18:36 +0300

    New db update

--- block_models.py
+++ block_models.py
@@ -2,10 +2,58 @@ from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
 from models import *
 
 
-class Block_texts(Base):
+class BlockTexts(Base):
     __tablename__ = 'block_texts'
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    block_type: Mapped[int] = mapped_column(ForeignKey('article_blocks.id'))
+    content: Mapped[str] = mapped_column(String(256), nullable=True)
+    class_name: Mapped[str] = mapped_column(String(300), nullable=True)
+
+
+class BlockImages(Base):
+    __tablename__ = 'block_images'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
     block_type:Mapped[int] = mapped_column(ForeignKey('article_blocks.id'))
-    content = Column(String(256), nullable=True)
-    class_name = Column(String(300), nullable=True)
+    src: Mapped[str] = mapped_column(String(256), nullable=True)
+    alt: Mapped[str] = mapped_column(String(256), nullable=True)
+    file: Mapped[int]
+    class_name = mapped_column(String(300), nullable=True)
+
+
+class BlockHeader(Base):
+    __tablename__ = 'block_texts'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    block_type: Mapped[int] = mapped_column(ForeignKey('article_blocks.id'))
+    content: Mapped[str] = mapped_column(String(256), nullable=True)
+    class_name: Mapped[str] = mapped_column(String(300), nullable=True)
+
+
+class BlockVideo(Base):
+    __tablename__ = 'block_images'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    block_type: Mapped[int] = mapped_column(ForeignKey('article_blocks.id'))
+    src: Mapped[str] = mapped_column(String(256), nullable=True)
+    type: Mapped[str] = mapped_column(String(256), nullable=True)
+    file: Mapped[int]
+    class_name = mapped_column(String(300), nullable=True)
+    controls: Mapped[bool]
+    autoplay: Mapped[bool]
+
+class BlockList(Base):
+    __tablename__ = 'block_lists'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    block_type: Mapped[int] = mapped_column(ForeignKey('article_blocks.id'))
+    items: Mapped[List['ListsItems']] = relationship()
+
+class ListsItems(Base):
+    __tablename__ = 'lists_items'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    list_id: Mapped[int] = mapped_column(ForeignKey('block_lists.id'))
+    content: Mapped[str] = mapped_column(String(1000), nullable=True)
+
