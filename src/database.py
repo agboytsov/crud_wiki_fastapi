9471e8b83cf434f58b60f921ef390b7c584e5172
agboytsov <133656833+agboytsov@users.noreply.github.com>
2024-03-21 16:44:30 +0300

    migration to postgres

--- src/database.py
+++ src/database.py
@@ -1,10 +1,23 @@
 # from __future__ import annotations
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-db = "sqlite:///new_1.db"
+db_user = os.getenv('DB_USER')
+db_name = os.getenv('DB_NAME')
+db_password = os.getenv('DB_PASSWORD')
+db_server = os.getenv('DB_SERVER')
+db_port = os.getenv('DB_PORT')
+
+
+
+# db = "sqlite:///new_1.db"
+db = f"postgresql+psycopg2://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
+
 engine = create_engine(db, echo=True)
+Session = sessionmaker(bind=engine)
 
 
 class Base(DeclarativeBase):
